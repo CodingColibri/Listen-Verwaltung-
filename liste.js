3b748d7aa484112e43eda40ff616a748f4ce60ec
Linda <linda.theresia@live.de>
2019-10-25 23:28:36 +0200

    Listenverwaltung in JS angefangen

--- liste.js
+++ liste.js
@@ -6,118 +6,144 @@ removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="
 var newTask = document.querySelector('#new-task');
 var addTaskBtn = document.querySelector('#addTask');
 
+var newList = document.querySelector('#new-list')
+var addListBtn = document.querySelector('#addList')
+
+
 var toDoUl = document.querySelector(".todo-list ul");
-var completeUl =  document.querySelector(".complete-list ul");
+var completeUl = document.querySelector(".complete-list ul");
+var ToDoListen = document.querySelector(".listen ul")
 
 
-//CREATE FUNCTIONS
+//CREATE FUNCTION
 
 //CREATING THE ACTUAL TASK LIST ITEM
-var createNewTask = function(task){
- 
-  //SET UP THE NEW LIST ITEM
-  //creating li, input and label 
-  var listItem = document.createElement("li"); //<li>
-  var checkBox = document.createElement("input"); //checkbox
-  var label = document.createElement("label"); // <label>
-  
-  
-  //PULL THE INPUTED TEXT INTO LABEL
-  //Inhalt wird durch den in der Variable task gespeicherten Text ersetzt
-  label.innerText = task;
-  
-  //Eigenschaften  hinzufügen 
-  checkBox.type = "checkbox";
-  
-  //ADD ITEMS TO THE LI
-  listItem.appendChild(checkBox);
-  listItem.appendChild(label);
-  //EVERYTHING PUT TOGETHER
-  return listItem;  
-  
+var createNewTask = function(task) {
+
+    //Neues To-do in der Liste
+    var listItem = document.createElement("li"); //<li>
+    var checkBox = document.createElement("input"); //checkbox
+    var label = document.createElement("label"); // <label>
+
+    //PULL THE INPUTED TEXT INTO LABEL
+    //Inhalt wird durch den in der Variable task gespeicherten Text ersetzt
+    label.innerText = task;
+
+
+    //Eigenschaften  hinzufügen 
+    checkBox.type = "checkbox";
+
+    //ADD ITEMS TO THE LI
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+    //EVERYTHING PUT TOGETHER
+    return listItem;
+
 };
+
+var createNewList = function(liste) {
+
+    //Neue Liste 
+    var listenobjekt = document.createElement("li");
+    var inhalt = document.createElement("label");
+
+    inhalt.innerText = liste;
+
+    //Neue Liste hinzufügen
+    listenobjekt.appendChild(label);
+    return listenobjekt;
+
+}
 
 //ADD THE NEW TASK INTO ACTUAL INCOMPLETE LIST
-var addTask = function(){
-  //console.log("Adding task...");
-  //FOR CLARITY, GRAB THE INPUTTED TEXT AND STORE IT IN A VAR
-  var listItem = createNewTask(newTask.value);
-  //ADD THE NEW LIST ITEM TO LIST
-  toDoUl.appendChild(listItem); 
-  //CLEAR THE INPUT
-  newTask.value="";
-  
-  //BIND THE NEW LIST ITEM TO THE INCOMPLETE LIST
-  bindIncompleteItems(listItem, completeTask);
+var addTask = function() {
+    //console.log("Adding task...");
+    //FOR CLARITY, GRAB THE INPUTTED TEXT AND STORE IT IN A VAR
+    var listItem = createNewTask(newTask.value);
+    //ADD THE NEW LIST ITEM TO LIST
+    toDoUl.appendChild(listItem);
+    //CLEAR THE INPUT
+    newTask.value = "";
+
+    //BIND THE NEW LIST ITEM TO THE INCOMPLETE LIST
+    bindIncompleteItems(listItem, completeTask);
 
 };
+var addList = function() {
+    var listenobjekt = createNewList(newList.value);
+    ToDoListen.appendChild(listenobjekt);
+    newList.value = "";
 
-var completeTask = function(){
-  
-  //GRAB THE CHECKBOX'S PARENT ELEMENT, THE LI IT'S IN
-  var listItem = this.parentNode;
-  
-  //CREATE AND INSERT THE DELETE BUTTON
-  var deleteBtn = document.createElement("button"); // <button>
-  deleteBtn.innerHTML = removeSVG; 
-  deleteBtn.className = "delete";
-  listItem.appendChild(deleteBtn);
-  
-  //SELECT THE CHECKBOX FROM THE COMPLETED CHECKBOX AND REMOVE IT
-  var checkBox = listItem.querySelector("input[type=checkbox]");
-  checkBox.remove();
-  
-  //PLACE IT INSIDE THE COMPLETED LIST
-  completeUl.appendChild(listItem); 
-  
-  //BIND THE NEW COMPLETED LIST
-  bindCompleteItems(listItem, deleteTask);
-  
+
+}
+
+var completeTask = function() {
+
+    //GRAB THE CHECKBOX'S PARENT ELEMENT, THE LI IT'S IN
+    var listItem = this.parentNode;
+
+    //CREATE AND INSERT THE DELETE BUTTON
+    var deleteBtn = document.createElement("button"); // <button>
+    deleteBtn.innerHTML = removeSVG;
+    deleteBtn.className = "delete";
+    listItem.appendChild(deleteBtn);
+
+    //SELECT THE CHECKBOX FROM THE COMPLETED CHECKBOX AND REMOVE IT
+    var checkBox = listItem.querySelector("input[type=checkbox]");
+    checkBox.remove();
+
+    //PLACE IT INSIDE THE COMPLETED LIST
+    completeUl.appendChild(listItem);
+
+    //BIND THE NEW COMPLETED LIST
+    bindCompleteItems(listItem, deleteTask);
+
 };
 
 
 
 //DELETE TASK FUNCTIONS
-var deleteTask = function(){
-  //console.log("Deleting task...");
-  
-  var listItem = this.parentNode;
-  var ul = listItem.parentNode;
-  
-  ul.removeChild(listItem);
+var deleteTask = function() {
+    //console.log("Deleting task...");
+
+    var listItem = this.parentNode;
+    var ul = listItem.parentNode;
+
+    ul.removeChild(listItem);
 };
 
 //A FUNCTION THAT BINDS EACH OF THE ELEMENTS THE INCOMPLETE LIST
-var bindIncompleteItems = function(taskItem, checkBoxClick){  
-  //console.log("Binding the incomplete list...");
-  
-  //BIND THE CHECKBOX TO A VAR
-  var checkBox = taskItem.querySelector("input[type=checkbox]");
-  
-  //SETUP EVENT LISTENER FOR THE CHECKBOX
-  checkBox.onchange = checkBoxClick;  
-}; 
+var bindIncompleteItems = function(taskItem, checkBoxClick) {
+    //console.log("Binding the incomplete list...");
+
+    //BIND THE CHECKBOX TO A VAR
+    var checkBox = taskItem.querySelector("input[type=checkbox]");
+
+    //SETUP EVENT LISTENER FOR THE CHECKBOX
+    checkBox.onchange = checkBoxClick;
+};
 
 
 //A FUNCTIONM THAT BINDS EACH OF THE ELEMTS IN THE COMPLETE LIST
-var bindCompleteItems = function(taskItem, deleteButtonPress){
-  //console.log("Binding the complete list...");
-  
-  //BIND THE DELETE BUTTON
-  var deleteButton = taskItem.querySelector(".delete");
-   
-  //WHEN THE DELETE BUTTIN IS PRESSED, RUN THE deleteTask function
-  deleteButton.onclick = deleteButtonPress;
-    
+var bindCompleteItems = function(taskItem, deleteButtonPress) {
+    //console.log("Binding the complete list...");
+
+    //BIND THE DELETE BUTTON
+    var deleteButton = taskItem.querySelector(".delete");
+
+    //WHEN THE DELETE BUTTIN IS PRESSED, RUN THE deleteTask function
+    deleteButton.onclick = deleteButtonPress;
+
 };
 
-for(var i=0; i < toDoUl.children.length; i++) {
-  bindIncompleteItems(toDoUl.children[i], completeTask);
+for (var i = 0; i < toDoUl.children.length; i++) {
+    bindIncompleteItems(toDoUl.children[i], completeTask);
 }
 
-for(var i=0; i < completeUl.children.length; i++) {
-  bindCompleteItems(completeUl.children[i], deleteTask);
+for (var i = 0; i < completeUl.children.length; i++) {
+    bindCompleteItems(completeUl.children[i], deleteTask);
 }
 
 
 addTaskBtn.addEventListener("click", addTask);
+addListBtn.addEventListener("click", addList);
